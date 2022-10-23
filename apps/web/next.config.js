07a8f158c0040f569d365f6fc1a9cd41cbcaa0d6
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-10-23 12:47:57 -0500

    I got it working, yay!

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,28 +1,5 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["ui", "models", "api-client"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // config.module.rules.push({
-    //   test: /\.(ts|js)x?$/,
-    //   exclude: /node_modules/,
-    //   use: {
-    //     loader: 'babel-loader',
-    //     options: {
-    //       plugins: [
-    //         "@babel/plugin-transform-react-jsx",
-    //       //   "@babel/preset-env",
-    //       //   "@babel/preset-react",
-    //       //   "@babel/preset-typescript",
-    //       ],
-    //     },
-    //   },
-    // });
-
-    // Important: return the modified config
-    return config
-  },
 });
