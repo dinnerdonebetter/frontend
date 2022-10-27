52bf4da42adb1ccf4000e8c4649c6288ccd7eb3e
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-10-26 19:36:54 -0500

    fix formatting

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path');
 const withTM = require('next-transpile-modules')(['ui', 'models', 'api-client']);
 
 module.exports = withTM({
   reactStrictMode: true,
-  output: "standalone",
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_API_ENDPOINT: 'https://api.prixfixe.dev',
   },
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-    transpilePackages: ["ui', 'models', 'api-client"],
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+    transpilePackages: ['ui', 'models', 'api-client'],
   },
 });
