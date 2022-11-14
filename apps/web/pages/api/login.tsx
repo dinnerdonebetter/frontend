85f1833d898b604f92acf015a272aed8196e4940
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-11-14 00:20:33 -0600

    add tracing to login route only

--- apps/web/pages/api/login.tsx
+++ apps/web/pages/api/login.tsx
@@ -4,9 +4,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { ServiceError, UserLoginInput, UserStatusResponse } from 'models';
 
 import { buildCookielessServerSideClient } from '../../src/client';
+import { serverSideTracer } from '../../src/tracer';
 import { processCookieHeader } from '../../src/auth';
 
 async function LoginRoute(req: NextApiRequest, res: NextApiResponse) {
+  const span = serverSideTracer.startSpan("LoginRoute");
+
   if (req.method === 'POST') {
     const input = req.body as UserLoginInput;
 
@@ -15,6 +18,7 @@ async function LoginRoute(req: NextApiRequest, res: NextApiResponse) {
     await pfClient
       .logIn(input)
       .then((result: AxiosResponse) => {
+        span.addEvent('response received');
         if (result.status === 205) {
           res.status(205).send('');
           return;
@@ -23,12 +27,15 @@ async function LoginRoute(req: NextApiRequest, res: NextApiResponse) {
         res.setHeader('Set-Cookie', processCookieHeader(result)).status(202).send('');
       })
       .catch((err: AxiosError<ServiceError>) => {
+        span.addEvent('error received');
         res.status(err.response?.status || 500).send('');
         return;
       });
   } else {
     res.status(405).send(`Method ${req.method} Not Allowed`);
   }
+
+  span.end();
 }
 
 export default LoginRoute;
