d67d674eb9f7598565eb60a1aa03556ae0896106
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-11-15 19:08:06 -0600

    some necessary changes

--- apps/web/src/client/index.ts
+++ apps/web/src/client/index.ts
@@ -6,38 +6,38 @@ import PrixFixeAPIClient from '@prixfixeco/api-client';
 import { cookieName } from '../constants';
 
 export const buildServerSideClient = (context: GetServerSidePropsContext): PrixFixeAPIClient => {
-  const apiClientID = process.env.NEXT_PUBLIC_API_ENDPOINT;
-  if (!apiClientID) {
-    throw new Error('no API client ID set!');
+  const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+  if (!apiEndpoint) {
+    throw new Error('no API endpoint set!');
   }
 
-  const pfClient = new PrixFixeAPIClient(apiClientID, context.req.cookies[cookieName]);
+  const pfClient = new PrixFixeAPIClient(apiEndpoint, context.req.cookies[cookieName]);
 
   return pfClient;
 };
 
 export const buildServerSideClientWithRawCookie = (cookie: string): PrixFixeAPIClient => {
-  const apiClientID = process.env.NEXT_PUBLIC_API_ENDPOINT;
-  if (!apiClientID) {
-    throw new Error('no API client ID set!');
+  const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+  if (!apiEndpoint) {
+    throw new Error('no API endpoint set!');
   }
 
   if (!cookie) {
     throw new Error('no cookie set!');
   }
 
-  const pfClient = new PrixFixeAPIClient(apiClientID, cookie);
+  const pfClient = new PrixFixeAPIClient(apiEndpoint, cookie);
 
   return pfClient;
 };
 
 export const buildCookielessServerSideClient = (): PrixFixeAPIClient => {
-  const apiClientID = process.env.NEXT_PUBLIC_API_ENDPOINT;
-  if (!apiClientID) {
-    throw new Error('no API client ID set!');
+  const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+  if (!apiEndpoint) {
+    throw new Error('no API endpoint set!');
   }
 
-  const pfClient = new PrixFixeAPIClient(apiClientID);
+  const pfClient = new PrixFixeAPIClient(apiEndpoint);
 
   return pfClient;
 };
