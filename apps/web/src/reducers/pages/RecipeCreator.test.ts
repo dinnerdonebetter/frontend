39991e831d857241a0f9e8a4757b7610199d3ed1
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-12-12 22:54:07 -0600

    fix?

--- apps/web/src/reducers/pages/RecipeCreator.test.ts
+++ apps/web/src/reducers/pages/RecipeCreator.test.ts
@@ -19,3 +19,175 @@ test('should be able to change the description', () => {
 
   expect(newState.recipe.description).toEqual(newDescription);
 });
+
+test('should be able to toggle show all ingredients', () => {
+  //
+});
+
+test('should be able to toggle show all instruments', () => {
+  //
+});
+
+test('should be able to toggle showing advanced prep steps', () => {
+  //
+});
+
+test('should be able to update submission error', () => {
+  //
+});
+
+test('should be able to update the recipe source', () => {
+  //
+});
+
+test('should be able to update the number of yielded portions', () => {
+  //
+});
+
+test('should be able to add a step', () => {
+  //
+});
+
+test('should be able to toggle showing a step', () => {
+  //
+});
+
+test('should be able to remove a step', () => {
+  //
+});
+
+test('should be able to add an ingredient to a recipe step', () => {
+  //
+});
+
+test('should be able to add an instrument to a recipe step', () => {
+  //
+});
+
+test('should be able to remove an ingredient from a recipe step', () => {
+  //
+});
+
+test('should be able to remove an instrument from a recipe step', () => {
+  //
+});
+
+test('should be able to update a step preparation query', () => {
+  //
+});
+
+test('should be able to update the notes for a recipe step', () => {
+  //
+});
+
+test('should be able to update the ingredient query for a recipe step', () => {
+  //
+});
+
+test('should be able to update the product measurement unit query for a given step product', () => {
+  //
+});
+
+test('should be able to update the product measurement unit suggestions for a given step product', () => {
+  //
+});
+
+test('should be able to add a completion condition to a recipe step', () => {
+  //
+});
+
+test('should be able to update the completion condition ingredient state query for a given recipe step completion condition', () => {
+  //
+});
+
+test('should be able to update the completion condition ingredient state suggestions for a given recipe step completion condition', () => {
+  //
+});
+
+test('should be able to update the valid ingredient state for a given recipe step completion condition', () => {
+  //
+});
+
+test('should be able to remove a recipe step completion condition', () => {
+  //
+});
+
+test('should be able to update the measurement unit for a given recipe step product', () => {
+  //
+});
+
+test('should be able to update the type of a recipe step product', () => {
+  //
+});
+
+test('should be able to update the measurement unit query of a given recipe step ingredient', () => {
+  //
+});
+
+test('should be able to udpate the measurement unit suggestions for a given recipe step ingredient', () => {
+  //
+});
+
+test('should be able to update the measurement unit for a given reciep step measurement unit', () => {
+  //
+});
+
+test('should be able to update the ingredient query for a given recipe step', () => {
+  //
+});
+
+test('should be able to update the instrument suggestions for a given recipe step', () => {
+  //
+});
+
+test('should be able to update the preparation suggestions for a given recipe step', () => {
+  //
+});
+
+test('should be able to update the minimum quantity of a given recipe step ingredient', () => {
+  //
+});
+
+test('should be able to update the maximum quantity of a given recipe step ingredient', () => {
+  //
+});
+
+test('should be able to update the minimum quantity of a given recipe step product', () => {
+  //
+});
+
+test('should be able to update the maximum quantity of a given recipe step product', () => {
+  //
+});
+
+test('should be able to update the minimum quantity of a given recipe step instrument', () => {
+  //
+});
+
+test('should be able to update the maximum quantity of a given recipe step instrument', () => {
+  //
+});
+
+test('should be able to update the preparation for a recipe step', () => {
+  //
+});
+
+test('should be able to update the name of a recipe step product', () => {
+  //
+});
+
+test('should be able to toggle ranged status for a given recipe step ingredient', () => {
+  //
+});
+
+test('should be able to toggle ranged status for a given recipe step instrument', () => {
+  //
+});
+
+test('should be able to toggle whether or not a product is ranged', () => {
+  //
+});
+
+test('should be able to toggle manual product naming', () => {
+  //
+});
