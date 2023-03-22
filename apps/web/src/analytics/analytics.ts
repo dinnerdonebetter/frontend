ad2bc9aaf97603d5b838f96c24c4174a4f695a3a
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-03-22 18:19:52 -0500

    okay

--- apps/web/src/analytics/analytics.ts
+++ apps/web/src/analytics/analytics.ts
@@ -2,6 +2,6 @@ import { Analytics } from '@segment/analytics-node';
 import { AnalyticsBrowser } from '@segment/analytics-next';
 
 export const browserSideAnalytics = AnalyticsBrowser.load({
-  writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || '',
+  writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || 'never_mind',
 });
-export const serverSideAnalytics = new Analytics({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || '' });
+export const serverSideAnalytics = new Analytics({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || 'never_mind' });
