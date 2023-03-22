69118ee059bf9d24c8ecf273a33c9952f6630429
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-03-22 18:22:42 -0500

    god damnit

--- apps/web/src/analytics/analytics.ts
+++ apps/web/src/analytics/analytics.ts
@@ -4,4 +4,7 @@ import { AnalyticsBrowser } from '@segment/analytics-next';
 export const browserSideAnalytics = AnalyticsBrowser.load({
   writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || 'never_mind',
 });
-export const serverSideAnalytics = new Analytics({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || 'never_mind' });
+
+export const serverSideAnalytics = new Analytics({
+  writeKey: process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || 'never_mind',
+});
