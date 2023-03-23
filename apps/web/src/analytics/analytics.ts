e2f6391cdcc7198d1d6490ecda5e8ef3bc7eb61d
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-03-22 21:30:44 -0500

    fixes

--- apps/web/src/analytics/analytics.ts
+++ apps/web/src/analytics/analytics.ts
@@ -9,76 +9,76 @@ interface analyticsWrapper {
 
 class serverAnalyticsWrapper {
   noopMode: boolean = false;
-  analytics: Analytics;
+  analytics?: Analytics;
 
   constructor() {
     const apiKey = process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || '';
     if (apiKey === '') {
       this.noopMode = true;
+    } else {
+      this.analytics = new Analytics({ writeKey: apiKey });
     }
-
-    this.analytics = new Analytics({ writeKey: apiKey });
   }
 
   track(userId: string, event: string, properties: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.track({ event, properties, userId });
+      this.analytics?.track({ event, properties, userId });
     } else {
-      console.debug("server analytics wrapper had 'track' called while operating in noop mode")
+      console.debug("server analytics wrapper had 'track' called while operating in noop mode");
     }
   }
 
   page(userId: string, name: string, properties: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.page({ name, properties, userId });
+      this.analytics?.page({ name, properties, userId });
     } else {
-      console.debug("server analytics wrapper had 'page' called while operating in noop mode")
+      console.debug("server analytics wrapper had 'page' called while operating in noop mode");
     }
   }
 
   identify(userId: string, traits: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.identify({ userId, traits });
+      this.analytics?.identify({ userId, traits });
     } else {
-      console.debug("server analytics wrapper had 'identify' called while operating in noop mode")
+      console.debug("server analytics wrapper had 'identify' called while operating in noop mode");
     }
   }
 }
 
 class browserAnalyticsWrapper {
   noopMode: boolean = false;
-  analytics: AnalyticsBrowser;
+  analytics?: AnalyticsBrowser;
 
   constructor() {
     const apiKey = process.env.NEXT_PUBLIC_SEGMENT_API_TOKEN || '';
     if (apiKey === '') {
       this.noopMode = true;
+    } else {
+      this.analytics = AnalyticsBrowser.load({ writeKey: apiKey });
     }
-
-    this.analytics = AnalyticsBrowser.load({ writeKey: apiKey });
   }
 
   track(userId: string, event: string, properties: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.track(event, properties);
+      this.analytics?.track(event, properties);
     } else {
-      console.debug("browser analytics wrapper had 'track' called while operating in noop mode")
+      console.debug("browser analytics wrapper had 'track' called while operating in noop mode");
     }
   }
 
   page(_userId: string, name: string, properties: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.page({ name, properties });
+      this.analytics?.page({ name, properties });
     } else {
-      console.debug("browser analytics wrapper had 'page' called while operating in noop mode")
+      console.debug("browser analytics wrapper had 'page' called while operating in noop mode");
     }
   }
 
   identify(userId: string, traits: Record<string, any>) {
     if (!this.noopMode) {
-      this.analytics.identify(userId, { traits });
+      this.analytics?.identify(userId, { traits });
     } else {
-      console.debug("browser analytics wrapper had 'identify' called while operating in noop mode")
+      console.debug("browser analytics wrapper had 'identify' called while operating in noop mode");
     }
   }
 }
