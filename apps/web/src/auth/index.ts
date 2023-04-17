6e2e28f20a8e0f7ef82c8559b0ad8359bf2d22c5
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-04-17 04:26:07 -0500

    try just modifying the cookie instead

--- apps/web/src/auth/index.ts
+++ apps/web/src/auth/index.ts
@@ -1,5 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { NextApiRequestCookies } from 'next/dist/server/api-utils';
+import { buildServerSideLogger } from '../logger';
 
 import { serverSideTracer } from '../tracer';
 
@@ -11,38 +12,53 @@ export interface sessionAuth {
   householdID: string;
 }
 
-export function processWebappCookieHeader(result: AxiosResponse, userID: string, householdID: string): string[] {
+const logger = buildServerSideLogger('cookie-manip');
+
+export function processWebappCookieHeader(result: AxiosResponse, userID: string, householdID: string): string {
   const span = serverSideTracer.startSpan('processCookieHeader');
 
-  let cookieHeader = result.headers['set-cookie']?.[0] ?? '';
-  if (!cookieHeader) {
+  let apiCookie = result.headers['set-cookie']?.[0] ?? '';
+  if (!apiCookie) {
     throw new Error('missing cookie header');
   }
 
+  const originalCookie = apiCookie;
+
   if (process.env.REWRITE_COOKIE_HOST_FROM && process.env.REWRITE_COOKIE_HOST_TO) {
-    cookieHeader = cookieHeader.replace(process.env.REWRITE_COOKIE_HOST_FROM, process.env.REWRITE_COOKIE_HOST_TO);
+    apiCookie = apiCookie.replace(process.env.REWRITE_COOKIE_HOST_FROM, process.env.REWRITE_COOKIE_HOST_TO);
+    logger.info(`rewrote cookie host`);
     span.addEvent('cookie host rewritten');
   }
 
   if (process.env.REWRITE_COOKIE_SECURE === 'true') {
-    cookieHeader = cookieHeader.replace('Secure; ', '');
+    apiCookie = apiCookie.replace('Secure; ', '');
+    logger.info(`rewrote secure cookie string`);
     span.addEvent('secure setting rewritten in cookie');
   }
 
-  const webappCookieParts = cookieHeader.split('; ');
-  webappCookieParts[0] = `${WebappCookieName}=${Buffer.from(
-    JSON.stringify({ userID, householdID } as sessionAuth),
-    'ascii',
-  ).toString('base64')}`;
-  const webappCookie = webappCookieParts.join('; ');
+  const webappCookieParts = apiCookie.split('; ');
+  webappCookieParts.push(
+    `${WebappCookieName}=${Buffer.from(JSON.stringify({ userID, householdID } as sessionAuth), 'ascii').toString(
+      'base64',
+    )}`,
+  );
+  apiCookie = webappCookieParts.join('; ');
+
+  logger.info(`processWebappCookieHeader: ${originalCookie} -> ${apiCookie}`);
 
   span.end();
-  return [cookieHeader, webappCookie];
+  return apiCookie;
 }
 
 export const extractUserInfoFromCookie = (cookies: NextApiRequestCookies): sessionAuth => {
-  const data = (cookies['prixfixe_webapp'] || Buffer.from('e30=', 'ascii')).toString();
+  const cookieParts = cookies[cookieName]?.split('; ') ?? [];
+  const sessionAuthCookie = cookieParts.find((part) => part.startsWith(WebappCookieName));
+  if (!sessionAuthCookie) {
+    return {} as sessionAuth;
+  }
 
-  const userSessionData = JSON.parse(Buffer.from(data, 'base64').toString('ascii')) as sessionAuth;
-  return userSessionData;
+  logger.info('cookieParts: ', cookieParts.join(', '));
+
+  const data = sessionAuthCookie.split('=')[1];
+  return JSON.parse(Buffer.from(data, 'base64').toString('ascii')) as sessionAuth;
 };
