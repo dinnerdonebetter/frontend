e34aa09acaf24138e71882e885ad251ab596d7a3
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-04-18 08:28:20 -0500

    attempt to fix logger problems

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -1,19 +1,33 @@
 import { Logger } from 'tslog';
 
-export const buildServerSideLogger = (name: string): Logger => {
-  const pfLogger = new Logger({
-    type: 'json',
-    name: name,
-    minLevel: 'debug',
-    colorizePrettyLogs: true,
-    dateTimePattern: 'year-month-day hour:minute:second.millisecond',
-    dateTimeTimezone: 'America/Chicago',
-    prefix: [``],
-    displayDateTime: true,
-    displayLogLevel: true,
-    displayLoggerName: true,
-    displayFilePath: 'hidden',
-    displayFunctionName: false,
+export const buildServerSideLogger = (name: string): Logger<void> => {
+  const pfLogger = new Logger<void>({
+    prettyLogTemplate: "{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}\t[{{filePathWithLine}}{{name}}]\t",
+    prettyErrorTemplate: "\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}",
+    prettyErrorStackTemplate: "  • {{fileName}}\t{{method}}\n\t{{filePathWithLine}}",
+    prettyErrorParentNamesSeparator: ":",
+    prettyErrorLoggerNameDelimiter: "\t",
+    stylePrettyLogs: true,
+    prettyLogTimeZone: "UTC",
+    prettyLogStyles: {
+      logLevelName: {
+        "*": ["bold", "black", "bgWhiteBright", "dim"],
+        SILLY: ["bold", "white"],
+        TRACE: ["bold", "whiteBright"],
+        DEBUG: ["bold", "green"],
+        INFO: ["bold", "blue"],
+        WARN: ["bold", "yellow"],
+        ERROR: ["bold", "red"],
+        FATAL: ["bold", "redBright"],
+      },
+      dateIsoStr: "white",
+      filePathWithLine: "white",
+      name: ["white", "bold"],
+      nameWithDelimiterPrefix: ["white", "bold"],
+      nameWithDelimiterSuffix: ["white", "bold"],
+      errorName: ["bold", "bgRedBright", "whiteBright"],
+      fileName: ["yellow"],
+    },
   });
 
   return pfLogger;
