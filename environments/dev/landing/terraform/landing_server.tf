655e6038c00f56a01b78f56ae8685daba37db441
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-07-16 20:58:56 -0500

    remove monitoring from dev environments

--- environments/dev/landing/terraform/landing_server.tf
+++ environments/dev/landing/terraform/landing_server.tf
@@ -196,116 +196,116 @@ resource "google_cloud_run_domain_mapping" "landing_base_domain_mapping" {
   }
 }
 
-resource "google_monitoring_service" "landing_service" {
-  service_id   = "landing-service"
-  display_name = "Landing Service"
+# resource "google_monitoring_service" "landing_service" {
+#   service_id   = "landing-service"
+#   display_name = "Landing Service"
 
-  basic_service {
-    service_type = "CLOUD_RUN"
-    service_labels = {
-      service_name = google_cloud_run_service.landing_server.name
-      location     = local.gcp_region
-    }
-  }
-}
+#   basic_service {
+#     service_type = "CLOUD_RUN"
+#     service_labels = {
+#       service_name = google_cloud_run_service.landing_server.name
+#       location     = local.gcp_region
+#     }
+#   }
+# }
 
-resource "google_monitoring_slo" "landing_server_latency_slo" {
-  service = google_monitoring_service.landing_service.service_id
+# resource "google_monitoring_slo" "landing_server_latency_slo" {
+#   service = google_monitoring_service.landing_service.service_id
 
-  slo_id          = "landing-server-latency-slo"
-  goal            = 0.999
-  calendar_period = "DAY"
-  display_name    = "Landing Server Latency"
+#   slo_id          = "landing-server-latency-slo"
+#   goal            = 0.999
+#   calendar_period = "DAY"
+#   display_name    = "Landing Server Latency"
 
-  basic_sli {
-    latency {
-      threshold = "1s"
-    }
-  }
-}
+#   basic_sli {
+#     latency {
+#       threshold = "1s"
+#     }
+#   }
+# }
 
-resource "google_monitoring_slo" "landing_server_availability_slo" {
-  service = google_monitoring_service.landing_service.service_id
+# resource "google_monitoring_slo" "landing_server_availability_slo" {
+#   service = google_monitoring_service.landing_service.service_id
 
-  slo_id          = "landing-server-availability-slo"
-  goal            = 0.999
-  calendar_period = "DAY"
-  display_name    = "Landing Server Availability"
+#   slo_id          = "landing-server-availability-slo"
+#   goal            = 0.999
+#   calendar_period = "DAY"
+#   display_name    = "Landing Server Availability"
 
-  basic_sli {
-    availability {
-      enabled = true
-    }
-  }
-}
+#   basic_sli {
+#     availability {
+#       enabled = true
+#     }
+#   }
+# }
 
-resource "google_monitoring_uptime_check_config" "landing_uptime_check" {
-  display_name = "landing-server-uptime-check"
-  timeout      = "60s"
-  period       = "300s"
+# resource "google_monitoring_uptime_check_config" "landing_uptime_check" {
+#   display_name = "landing-server-uptime-check"
+#   timeout      = "60s"
+#   period       = "300s"
 
-  http_check {
-    path         = "/"
-    port         = "443"
-    use_ssl      = true
-    validate_ssl = true
-  }
+#   http_check {
+#     path         = "/"
+#     port         = "443"
+#     use_ssl      = true
+#     validate_ssl = true
+#   }
 
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = local.project_id
-      host       = local.web_location
-    }
-  }
-}
+#   monitored_resource {
+#     type = "uptime_url"
+#     labels = {
+#       project_id = local.project_id
+#       host       = local.web_location
+#     }
+#   }
+# }
 
 
-resource "google_monitoring_alert_policy" "landing_server_latency_alert_policy" {
-  display_name = "Landing Server Latency Alert Policy"
-  combiner     = "OR"
+# resource "google_monitoring_alert_policy" "landing_server_latency_alert_policy" {
+#   display_name = "Landing Server Latency Alert Policy"
+#   combiner     = "OR"
 
-  conditions {
-    display_name = "request latency"
-    condition_monitoring_query_language {
-      duration = ""
-      query    = <<END
-        fetch uptime_url
-        | metric 'monitoring.googleapis.com/uptime_check/request_latency'
-        | filter (metric.checked_resource_id == 'www.dinnerdonebetter.dev')
-        | group_by 5m, [value_request_latency_max: max(value.request_latency)]
-        | every 5m
-        | group_by [], [value_request_latency_max_max: max(value_request_latency_max)]
-        | group_by [],
-            [value_request_latency_max_max_mean: mean(value_request_latency_max_max)]
-        | condition val() > 999 'ms'
-      END
-    }
-  }
-}
+#   conditions {
+#     display_name = "request latency"
+#     condition_monitoring_query_language {
+#       duration = ""
+#       query    = <<END
+#         fetch uptime_url
+#         | metric 'monitoring.googleapis.com/uptime_check/request_latency'
+#         | filter (metric.checked_resource_id == 'www.dinnerdonebetter.dev')
+#         | group_by 5m, [value_request_latency_max: max(value.request_latency)]
+#         | every 5m
+#         | group_by [], [value_request_latency_max_max: max(value_request_latency_max)]
+#         | group_by [],
+#             [value_request_latency_max_max_mean: mean(value_request_latency_max_max)]
+#         | condition val() > 999 'ms'
+#       END
+#     }
+#   }
+# }
 
-resource "google_monitoring_alert_policy" "landing_server_memory_usage_alert_policy" {
-  display_name = "Landing Server Memory Usage"
-  combiner     = "OR"
-  conditions {
-    display_name = "Landing Server Memory Utilization"
+# resource "google_monitoring_alert_policy" "landing_server_memory_usage_alert_policy" {
+#   display_name = "Landing Server Memory Usage"
+#   combiner     = "OR"
+#   conditions {
+#     display_name = "Landing Server Memory Utilization"
 
-    condition_threshold {
-      filter     = "resource.type = \"cloud_run_revision\" AND (resource.labels.service_name = \"landing-server\") AND metric.type = \"run.googleapis.com/container/memory/utilizations\""
-      duration   = "300s"
-      comparison = "COMPARISON_GT"
-      aggregations {
-        alignment_period   = "300s"
-        per_series_aligner = "ALIGN_PERCENTILE_99"
-      }
-      trigger {
-        count = 1
-      }
-      threshold_value = 0.8
-    }
-  }
+#     condition_threshold {
+#       filter     = "resource.type = \"cloud_run_revision\" AND (resource.labels.service_name = \"landing-server\") AND metric.type = \"run.googleapis.com/container/memory/utilizations\""
+#       duration   = "300s"
+#       comparison = "COMPARISON_GT"
+#       aggregations {
+#         alignment_period   = "300s"
+#         per_series_aligner = "ALIGN_PERCENTILE_99"
+#       }
+#       trigger {
+#         count = 1
+#       }
+#       threshold_value = 0.8
+#     }
+#   }
 
-  alert_strategy {
-    auto_close = "259200s"
-  }
-}
+#   alert_strategy {
+#     auto_close = "259200s"
+#   }
+# }
